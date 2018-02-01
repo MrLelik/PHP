32e7206af350504b30d7fc85515bc3fe549e69ce
MrLelik <lelikpunk@gmail.com>
2018-02-01 20:42:38 +0200

    connect msqli

--- testPHPandMySQL.loc/index.php
+++ testPHPandMySQL.loc/index.php
@@ -1,17 +1,8 @@
 <?php
 
-$link = mysqli_connect("localhost", "root", "", "test_db");
-
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+$connect = new mysqli('localhost','root','','dbname');
+if ($connect->connect_errno) {
+    die('Sorry! Database connection error.');
 }
 
-$query = "SELECT name FROM people";
-$result = mysqli_query($link, $query);
 
-
-while (false !== ($row = mysqli_fetch_array($result))) {
-    echo $row['name'];
-    echo '<br>';
-}
