68fea241dc59770adf0e0ff0a04cd0db24906fbb
MrLelik <lelikpunk@gmail.com>
2018-02-03 17:08:55 +0200

    new add

--- testPHPLoadFile.loc/index.php
+++ testPHPLoadFile.loc/index.php
@@ -1,4 +1,9 @@
-
+<?php
+$connect = new mysqli('localhost','root','','dbname');
+    if ($connect->connect_errno) {
+        die('Sorry! Database connection error.');
+    }
+?>
 
 <!doctype html>
 <html lang="en">
