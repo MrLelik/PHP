2fbe0bf348c5cecaf0855bd347b384b879051781
MrLelik <lelikpunk@gmail.com.ua>
2018-04-03 17:45:56 +0300

    new form tasks sourceit.loc

--- sourceit.loc/index.php
+++ sourceit.loc/index.php
@@ -1,2 +1,194 @@
-<?php
-phpinfo();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>SourceIT Hometask</title>
+    <style>
+        body {
+            background-color: #5e5e5e;
+        }
+        .buttons {
+            margin: 0 auto;
+            padding-left: 10%;
+            padding-top: 3%;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="buttons">
+                <a class="btn btn-success" href="tasks/task1/index.php">Hometask 1</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 2</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 3</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 4</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 5</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 6</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 7</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 8</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 9</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 10</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 11</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 12</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 13</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 14</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 15</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 16</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 17</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask 18</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div><div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+            <div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div><div class="buttons">
+                <a class="btn btn-success" href="#">Hometask #</a>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
