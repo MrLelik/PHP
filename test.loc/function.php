25bc9e34415040b47429152c6c16791cd985c673
MrLelik <lelikpunk@gmail.com.ua>
2018-04-06 20:55:30 +0300

    new function

---
+++ test.loc/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lelik
+ * Date: 06.04.18
+ * Time: 20:54
+ */
--- test.loc/index.php
+++ test.loc/index.php
@@ -1,74 +1,31 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
 
-class Message
+function myStrLen($str)
 {
-    public $text;
-    public $subject;
-    public $to;
-
-    public function load_text($filename)
-    {
-        $this->text = join('',file($filename));
+    $i = 0;
+    while ($str{$i}) {
+        $i++;
     }
-
-    public static function sayHi()
-    {
-        echo 'Hi my function sayHi';
-    }
-
-    public function send()
-    {
-        echo $this->to . '<br>';
-        echo $this->subject . '<br>';
-        echo $this->text . '<br>';
-//        mail($this->to, $this->subject, $this->text);
-    }
-
-    public function __construct($to, $subject, $text)
-    {
-        $this->to = $to;
-        $this->subject = $subject;
-        $this->text = $text;
-        $this->load_text($this->text);
-    }
+    echo $i;
 }
 
-$mes = new Message('user@server.ua', 'Hi', 'message.txt');
 
-//$mes->to = 'user@server.ua';
-//$mes->subject = 'Hi';
-//$mes->load_text('message.txt');
-
-$mes->send();
-
-Message::sayHi();
-
-// Тест Массива.
-$food = [
-    'Борщ' => 'красный',
-    'Сосисочки' => true
-];
-
-$food['чай'] = 'вдвоем';
-
-echo '<pre>';
-print_r($food);
+$string = 'Hello';
+myStrlen($string);
 
 
 
 
-?>
-</body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
 
 
