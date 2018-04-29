bd78496dc969fb02fe2b2cb174ed2444d0d2dd80
MrLelik <lelikpunk@gmail.com.ua>
2018-04-29 18:54:13 +0300

    delete and change some dir

--- mysite.loc/function.php
+++ mysite.loc/function.php
@@ -1,58 +1,41 @@
 <?php
-session_start();
-require_once 'functionScore.php';
 
-const LOGIN = 'lelik';
-const PASSWORD = 'qqq';
-const USEREMAIL = 'lelik@gmail.com';
-const ADMINEMAIL = 'admin@gmail.com';
-
-function login(array $post)
+function viewTitle()
 {
-    $check = null;
+    $titleHome = 'Custom Blog';
+    $titleAbout = 'Custom Blog - About';
+    $titleSamplePost = 'Custom Blog - Post';
+    $titleContact = 'Custom Blog - Contact';
 
-    if (isset($post['login']) && isset($post['password'])) {
-        if ($post['login'] == LOGIN && md5($post['password']) == md5(PASSWORD)) {
-            $check = true;
-        }
-    }
-
-    if ($check) {
-        $_SESSION['access'] = true;
-        $_SESSION['login'] = $post['login'];
-        $_SESSION['email'] = USEREMAIL;
-        header('Location: /my_shop.php');
-        exit();
-    } else {
-        $_SESSION['access'] = false;
-        header('Location: /access_denied.php');
-        exit();
+    switch ($_SERVER['REQUEST_URI']) {
+        case '/':
+            echo $titleHome;
+            break;
+        case '/about.php':
+            echo $titleAbout;
+            break;
+        case '/samplePost.php':
+            echo $titleSamplePost;
+            break;
+        case '/contact.php':
+            echo $titleContact;
+            break;
+        default:
+            echo 'Default case';
+            break;
     }
 }
 
-function sendMailOrder()
+function viewTitle2()
 {
-    $to = ADMINEMAIL;
-    $subject = 'the subject';
-    $message = $_SESSION['title'] . ' ' . $_SESSION['description'] . ' ' . $_SESSION['price'];
+    $titleHome = 'Custom Blog';
+    $titleAbout = 'Custom Blog - About';
+    $titleSamplePost = 'Custom Blog - Post';
+    $titleContact = 'Custom Blog - Contact';
 
-    mail($to, $subject, $message);
-    echo 'Заказ оформлен';
-}
+    if ($_SERVER['REQUEST_URI'] === '/') {
+        echo $titleHome;
+    } elseif (strpos($_SERVER['REQUEST_URI'], 'about' )) {
 
-function sendMailFeedback($post)
-{
-    $to = ADMINEMAIL;
-    $subject = $post['subjectFeedback'];
-    $message = 'Сообщение от ' . $post['nameFeedback'] . ' Комментарий ' . $post['commentFeedback'];
-
-    mail($to, $subject, $message);
-    echo 'Письмо отправлено';
-}
-
-function saveOrder(array $product)
-{
-    $_SESSION['title'] = $product['title'];
-    $_SESSION['description'] = $product['description'];
-    $_SESSION['price'] = $product['price'];
+    }
 }
